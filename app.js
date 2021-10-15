0e9e379cfeb607a11a77d386513df1c4bc5593ea
mjb4040 <michaeljamesblackwell.@gmail.com>
2021-10-15 17:55:27 -0400

    inquirer

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-
-const fs = require('fs');
-
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-  const [name, github] = profileDataArgs
-  fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is you name?'
+    }
+  ])
+  .then(answers => console.log(answers));
   
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+// const pageHTML = generatePage(name, github);
+
+                        // const profileDataArgs = process.argv.slice(2, process.argv.length);
+                        //   const [name, github] = profileDataArgs
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
+  
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+//   });
       
 
 
